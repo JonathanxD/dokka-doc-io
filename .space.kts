3798a65ed377a8c5e6ca5f7ba9cd1ed52c8babb4
Jonathan <joniweb01@gmail.com>
2021-05-21 19:42:49 -0300

    Add env to standard build

--- .space.kts
+++ .space.kts
@@ -1,5 +1,12 @@
 job("Build and run tests") {
-   gradlew("openjdk:15", "build")
+   gradlew("openjdk:15", "build") {
+      env["ORG_GRADLE_PROJECT_signingKey"] = Secrets("org_gradle_project_signingkey")
+      env["ORG_GRADLE_PROJECT_signingPassword"] = Secrets("org_gradle_project_signingpassword")
+      env["OSSRH_USERNAME"] = Secrets("ossrh_username")
+      env["OSSRH_PASSWORD"] = Secrets("ossrh_password")
+      env["GRADLE_PUBLISH_KEY"] = Secrets("gradle_publish_key")
+      env["GRADLE_PUBLISH_SECRET"] = Secrets("gradle_publish_secret")
+   }
 }
 
 job("Publish plugin to Gradle and Maven") {
@@ -18,6 +25,5 @@ job("Publish plugin to Gradle and Maven") {
       env["OSSRH_PASSWORD"] = Secrets("ossrh_password")
       env["GRADLE_PUBLISH_KEY"] = Secrets("gradle_publish_key")
       env["GRADLE_PUBLISH_SECRET"] = Secrets("gradle_publish_secret")
-
    }
 }
